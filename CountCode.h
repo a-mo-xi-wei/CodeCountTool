246fdb92368a467c0c14bcd66f953af22f2d47e2
WeiWang <1428206861@qq.com>
2024-10-15 16:12:41 +0800

    代码统计完成

---
+++ CountCode.h
@@ -0,0 +1,36 @@
+﻿#ifndef COUNTCODE_H
+#define COUNTCODE_H
+
+#include <QWidget>
+
+namespace Ui {
+class CountCode;
+}
+
+class CountCode : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CountCode(QWidget *parent = 0);
+    ~CountCode();
+
+private:
+    Ui::CountCode *ui;
+    QStringList listFile;
+
+private:
+    void initForm();
+    bool checkFile(const QString &fileName);
+    bool checkDir(const QString& dirName);
+    void countCode(const QString &filePath);
+    void countCode(const QStringList &files);
+    void countCode(const QString &fileName, int &lineCode, int &lineBlank, int &lineNotes);
+
+private slots:
+    void on_btnOpenFile_clicked();
+    void on_btnOpenPath_clicked();
+    void on_btnClear_clicked();
+};
+
+#endif // OUNTCODE_H
---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#pragma execution_character_set("utf-8")
+
+#include "CountCode.h"
+#include <QApplication>
+#pragma comment(linker,"/subsystem:windows /entry:mainCRTStartup")
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFont font;
+    font.setFamily("Microsoft Yahei");
+    font.setPixelSize(13);
+    a.setFont(font);
+
+    CountCode w;
+    w.setWindowTitle("代码行数统计（威桑：1428206861@qq.com）");
+    w.show();
+
+    return a.exec();
+}
